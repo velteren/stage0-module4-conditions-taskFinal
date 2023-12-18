47ba05a4fa5ab455ea27aad5d838e7bda3441163
Constantine <77935386+velteren@users.noreply.github.com>
2023-12-18 13:09:48 +0300

    feat: add solution

--- src/main/java/school/mjc/stage0/conditions/finalTask/CoordinatePane.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/CoordinatePane.java
@@ -2,5 +2,10 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class CoordinatePane {
     public void printQuadrant(int x, int y) {
+        if (x == 0 && y == 0) System.out.println("zero");
+        if (x >= 0 && y >= 0) System.out.println("first");
+        if (x >= 0 && y < 0) System.out.println("fourth");
+        if (y >= 0 && x < 0) System.out.println("second");
+        if (y < 0 && x < 0) System.out.println("third");
     }
 }
--- src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
@@ -1,7 +1,41 @@
 package school.mjc.stage0.conditions.finalTask;
 
 public class DaysInMonth {
+    public boolean isLeapYear(int year) {
+        boolean result = year % 100 == 0 && year % 400 == 0 ? true : false;
+        result = result == false ?
+                (year % 4 == 0 && year % 100 !=0) ? true : false :
+                (year % 4 == 0) ? true: false ;
+        return result;
+    }
     public void printDays(int year, int month) {
-
+        boolean isLeapYear = isLeapYear(year);
+        int days;
+        switch (month) {
+            case (1):
+            case (3):
+            case (5):
+            case (7):
+            case (8):
+            case (10):
+            case (12):
+                days = 31;
+                System.out.println(days);
+                break;
+            case (2):
+                days = isLeapYear ? 29 : 28;
+                System.out.println(days);
+                break;
+            case (4):
+            case (6):
+            case (9):
+            case (11):
+                days = 30;
+                System.out.println(days);
+                break;
+            default:
+                days = 0;
+                System.out.println("wrong number!");
+        }
     }
 }
